3d2285790745a682d004208a4d32c1fbe2c28dd0
mdbasheer333 <mdbasheer333@gmail.com>
2021-02-20 20:28:06 +0530

    commit on 'master'

--- cypress-react-unittest/src/components/test.cy-spec.js
+++ cypress-react-unittest/src/components/test.cy-spec.js
@@ -1,16 +1,28 @@
-
 import { mount } from '@cypress/react'
 import MyComponent from '../MyComponent'
 
-
 describe('HelloWorld component', () => {
-  it('works', () => {
-    mount(<MyComponent/>)
-    cy.get('button#abc').should('have.text','clicked 0 times')
-    cy.get('button#abc').click().should('have.text','clicked 1 times')
-    .click().should('have.text','clicked 2 times')
 
-    cy.get('button#xyz').click().should('have.text','clicked 3 times')
+  // beforeEach(() => {
+  //   mount(<MyComponent />)
+  // });
+
+  before(() => {
+    mount(<MyComponent />)
+  });
+
+  it('works1', () => {
+
+    cy.get('button#abc').should('have.text', 'clicked 0 times')
+    cy.get('button#abc').click().should('have.text', 'clicked 1 times')
+      .click().should('have.text', 'clicked 2 times')
 
   })
+
+  it('works2', () => {
+
+    cy.get('button#xyz').click().should('have.text', 'clicked 3 times')
+
+  })
+
 })
